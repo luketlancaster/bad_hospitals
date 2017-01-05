a94055869bb84dc5018c7464d482b49df9257873
Luke Lancaster <luketlancaster@gmail.com>
2017-01-05 15:32:38 -0600

    Pulls down data, munges, and displays on the page.
    Woo hoo!

--- index.php
+++ index.php
@@ -3,20 +3,37 @@
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
      // The request is using the GET method
   $state = $_POST['state'];
-  echo "You got it, dude, you're from $state";
+  echo get_hospitals($state);
 } else {
   exit();
 }
 
+function get_hospitals($state) {
+  $poor_preformers_url = "https://data.medicare.gov/resource/5u3e-gkdf.json?state=$state&payment_reduction=Yes";
+  $hospital_url = "https://data.medicare.gov/resource/rbry-mqwu.json?state=$state";
 
+  $hospitals = file_get_contents($hospital_url);
+  $poor_preformers = file_get_contents($poor_preformers_url);
+  //
+  $hospitals = json_decode($hospitals, true);
+  $poor_preformers = json_decode($poor_preformers, true);
 
-// Write a function that'll go out and do the work...
-// Endpoints to hit:
-// Hospital-Acquired Condition Reduction Program
-// https://data.medicare.gov/resource/5u3e-gkdf.json?state={state}&payment_reduction=Yes
-//
-// General hospital information
-// https://data.medicare.gov/resource/rbry-mqwu.json?state={state}
-//
-// Get the addresses out of the General Hospital Information array
-// and return the mushed data back to FE
+  $munged = hospital_munger($hospitals, $poor_preformers);
+
+  return json_encode($munged);
+}
+
+function hospital_munger($hospitals, $performances) {
+  foreach($hospitals as $h_key => $hospital) {
+    foreach($performances as $p_key => $performance) {
+      if ($performance['provider_id'] === $hospital['provider_id']) {
+        $performances[$p_key]['address'] = $hospital['address'];
+        $performances[$p_key]['zip_code'] = $hospital['zip_code'];
+        $performances[$p_key]['city'] = $hospital['city'];
+      }
+    }
+  }
+
+  return $performances;
+}
+
--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var states = {"AL":"Alabama","AK":"Alaska","AS":"American Samoa","AZ":"Arizona","AR":"Arkansas","CA":"California","CO":"Colorado","CT":"Connecticut","DE":"Delaware","DC":"District Of Columbia","FM":"Federated States Of Micronesia","FL":"Florida","GA":"Georgia","GU":"Guam","HI":"Hawaii","ID":"Idaho","IL":"Illinois","IN":"Indiana","IA":"Iowa","KS":"Kansas","KY":"Kentucky","LA":"Louisiana","ME":"Maine","MH":"Marshall Islands","MD":"Maryland","MA":"Massachusetts","MI":"Michigan","MN":"Minnesota","MS":"Mississippi","MO":"Missouri","MT":"Montana","NE":"Nebraska","NV":"Nevada","NH":"New Hampshire","NJ":"New Jersey","NM":"New Mexico","NY":"New York","NC":"North Carolina","ND":"North Dakota","MP":"Northern Mariana Islands","OH":"Ohio","OK":"Oklahoma","OR":"Oregon","PW":"Palau","PA":"Pennsylvania","PR":"Puerto Rico","RI":"Rhode Island","SC":"South Carolina","SD":"South Dakota","TN":"Tennessee","TX":"Texas","UT":"Utah","VT":"Vermont","VI":"Virgin Islands","VA":"Virginia","WA":"Washington","WV":"West Virginia","WI":"Wisconsin","WY":"Wyoming"}
+var states = {"AL":"Alabama","AK":"Alaska","AZ":"Arizona","AR":"Arkansas","CA":"California","CO":"Colorado","CT":"Connecticut","DE":"Delaware","DC":"District Of Columbia","FL":"Florida","GA":"Georgia","HI":"Hawaii","ID":"Idaho","IL":"Illinois","IN":"Indiana","IA":"Iowa","KS":"Kansas","KY":"Kentucky","LA":"Louisiana","ME":"Maine","MD":"Maryland","MA":"Massachusetts","MI":"Michigan","MN":"Minnesota","MS":"Mississippi","MO":"Missouri","MT":"Montana","NE":"Nebraska","NV":"Nevada","NH":"New Hampshire","NJ":"New Jersey","NM":"New Mexico","NY":"New York","NC":"North Carolina","ND":"North Dakota","OH":"Ohio","OK":"Oklahoma","OR":"Oregon","PA":"Pennsylvania","PR":"Puerto Rico","RI":"Rhode Island","SC":"South Carolina","SD":"South Dakota","TN":"Tennessee","TX":"Texas","UT":"Utah","VT":"Vermont","VA":"Virginia","WA":"Washington","WV":"West Virginia","WI":"Wisconsin","WY":"Wyoming"}
 
 
 
@@ -22,10 +22,29 @@ $(document).ready(function() {
       type: "POST",
       url: "index.php",
       data: {state: state},
-      success: function(data) {
-        console.log(data);
-      }
+      success: attachHospitals
     });
+  }
+
+  function attachHospitals(hospitals) {
+    var hospitals = $.parseJSON(hospitals);
+    var hospitalDiv = $("#hospitals");
+    var hospitalCountDiv = $("#hospitalCount");
+
+    hospitalDiv.html('');
+    hospitalCountDiv.html('<h2># of Scary Hospitals: ' + hospitals.length + '<h2>');
+    hospitals.forEach(function(hospital) {
+      hospitalDiv.append(createTemplate(hospital));
+    });
+  }
+
+  function createTemplate(hospital) {
+    var html = "<div class='twelve columns hospital_box'>";
+        html += "<h3>" + hospital.hospital_name + "</h3>";
+        html += "<p>" + hospital.address + ", " + hospital.city + ", ";
+        html += hospital.state + " " + hospital.zip_code + "</p>";
+    
+    return html;
   }
 
 });
