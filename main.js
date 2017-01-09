0bda159431d257c5db5fc33e967b8602ddc9d4bf
Luke Lancaster <luketlancaster@gmail.com>
2017-01-09 08:59:51 -0600

    Boldify the number count

--- main.js
+++ main.js
@@ -32,7 +32,7 @@ $(document).ready(function() {
     var hospitalCountDiv = $("#hospitalCount");
 
     hospitalDiv.html('');
-    hospitalCountDiv.html('<h2>There are ' + hospitals.length + ' Scary Hospitals in ' + stateSelect.val() +'<h2>');
+    hospitalCountDiv.html('<h2>There are <b>' + hospitals.length + '</b> Scary Hospitals in ' + stateSelect.val() +'<h2>');
     hospitals.forEach(function(hospital) {
       hospitalDiv.append(createTemplate(hospital));
     });
