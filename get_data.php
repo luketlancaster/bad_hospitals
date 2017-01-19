340af799a1c8f71f5a411f25fe3614651145d111
Luke Lancaster <luketlancaster@gmail.com>
2017-01-18 20:28:53 -0600

    Moves to using local DB to save and store things
    This should speed up loading. Also using moustache
    to render templates on the server, because why not?
    
    TODO:
    * The `get_data.php` script will need to be run on whatever
    env the app lives on.
    * Pull out DB secrets (currently just homestead defaults) and
    store in ENV variables
    * Create script to get lat/lon for hospitals and store on hospitals
    table
    * Add in schema for DB and instructions for deployment
    * MAPS!!! It would be great to have a map load these up

---
+++ get_data.php
@@ -0,0 +1,91 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+
+$username = 'homestead';
+$password = 'secret';
+
+$easy_db = \ParagonIE\EasyDB\Factory::create(
+  'pgsql:host=localhost;dbname=homestead',
+  $username,
+  $password
+);
+
+$socrata = new Socrata('data.medicare.gov');
+//$hospitals = $socrata->get('/resource/rbry-mqwu.json?$limit=10000000');
+
+$bad_ones = $socrata->get('/resource/5u3e-gkdf.json?$limit=100000000');
+
+
+/*
+foreach ($bad_ones as $b) {
+  if (isset($b['mrsa_footnote'])) unset($b['mrsa_footnote']);
+  if (isset($b['ssi_score_footnote'])) unset($b['ssi_score_footnote']);
+  if (isset($b['cauti_score_footnote'])) unset($b['cauti_score_footnote']);
+  if (isset($b['clabsi_score_footnote'])) unset($b['clabsi_score_footnote']);
+  if (isset($b['cdi_footnote'])) unset($b['cdi_footnote']);
+  if (isset($b['domain_2_score_footnote'])) unset($b['domain_2_score_footnote']);
+  if (isset($b['ahrq_psi_90_score_footnote'])) unset($b['ahrq_psi_90_score_footnote']);
+  if (isset($b['domain_1_score_footnote'])) unset($b['domain_1_score_footnote']);
+  if (isset($b['footnotes'])) unset($b['footnotes']);
+  if (isset($b['payment_reduction_footnote'])) unset($b['payment_reduction_footnote']);
+  $easy_db->insert('problems', $b);
+}
+
+$count = 0;
+foreach ($hospitals as $h) {
+  if (isset($h['efficient_use_of_medical_imaging_national_comparison_footnote'])) unset($h['efficient_use_of_medical_imaging_national_comparison_footnote']);
+  if (isset($h['safety_of_care_national_comparison_footnote'])) unset($h['safety_of_care_national_comparison_footnote']);
+  if (isset($h['patient_experience_national_comparison_footnote'])) unset($h['patient_experience_national_comparison_footnote']);
+  if (isset($h['hospital_overall_rating_footnote'])) unset($h['hospital_overall_rating_footnote']);
+  if (isset($h['mortality_national_comparison_footnote'])) unset($h['mortality_national_comparison_footnote']);
+  if (isset($h['readmission_national_comparison_footnote'])) unset($h['readmission_national_comparison_footnote']);
+  if (isset($h['effectiveness_of_care_national_comparison_footnote'])) unset($h['effectiveness_of_care_national_comparison_footnote']);
+  if (isset($h['timeliness_of_care_national_comparison_footnote'])) unset($h['timeliness_of_care_national_comparison_footnote']);
+  $easy_db->insert('hospitals', $h);
+  $count++;
+}
+
+$poor_preformers_url = "https://data.medicare.gov/resource/5u3e-gkdf.json?payment_reduction=Yes";
+$hospital_url = "https://data.medicare.gov/resource/rbry-mqwu.json";
+
+$hospitals = file_get_contents($hospital_url);
+$poor_preformers = file_get_contents($poor_preformers_url);
+//
+$hospitals = json_decode($hospitals, true);
+$poor_preformers = json_decode($poor_preformers, true);
+
+//$munged = hospital_munger($hospitals, $poor_preformers);
+
+function hospital_munger($hospitals, $performances) {
+  foreach($hospitals as $h_key => $hospital) {
+    foreach($performances as $p_key => $performance) {
+      if ($performance['provider_id'] === $hospital['provider_id']) {
+        //if(empty($hospital['address'])) {
+         // $performances[$p_key]['address'] = '';
+        //} else {
+          $performances[$p_key]['address'] = $hospital['address'];
+        //}
+
+        //if(empty($hospital['zip_code'])) {
+         // $performances[$p_key]['zip'] = '';
+        //} else {
+          $performances[$p_key]['zip'] = $hospital['zip_code'];
+        //}
+
+        //if(empty($hospital['city'])) {
+         // $performances[$p_key]['city'] = '';
+        
+        //} else {
+          $performances[$p_key]['city'] = $hospital['city'];
+        //}
+      }
+      $easy_db->insert('hospitals', $performances[$p_key]);
+    }
+  }
+
+  return $performances;
+}
+$count = 0;
+
+ */
--- index.php
+++ index.php
@@ -1,13 +1,56 @@
 <?php
 
+require __DIR__ . '/vendor/autoload.php';
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-     // The request is using the GET method
   $state = $_POST['state'];
-  echo get_hospitals($state);
+  $good_or_bad = $_POST['good_or_bad'];
+  echo render_hos(get_hos($state, $good_or_bad));
 } else {
   exit();
 }
 
+
+
+function get_hos($state, $reduction) {
+  $sql = 'select distinct
+            p.provider_id,
+            p.payment_reduction,
+            h.hospital_name as hospital_name,
+            h.address as address,
+            h.city as city,
+            h.state as state,
+            h.zip_code as zip_code,
+            h.phone_number as phone_number
+          from problems as p
+          join hospitals as h on (p.provider_id = h.provider_id)
+          where h.state = ?
+          and p.payment_reduction = ?';
+
+
+  $username = 'homestead';
+  $password = 'secret';
+
+  $easy_db = \ParagonIE\EasyDB\Factory::create(
+    'pgsql:host=localhost;dbname=homestead',
+    $username,
+    $password
+  );
+
+  return $easy_db->safeQuery($sql, [$state, $reduction]);
+}
+
+function render_hos($rows) {
+  $m = new Mustache_Engine(array(
+      'loader' => new Mustache_Loader_FilesystemLoader(dirname(__FILE__) . '/views'),
+    ));
+
+  $mu = new Mustache_Engine;
+
+  return $m->render('hospitals', ["hospitals" => $rows]);
+}
+
+/*
 function get_hospitals($state) {
   $poor_preformers_url = "https://data.medicare.gov/resource/5u3e-gkdf.json?state=$state&payment_reduction=Yes";
   $hospital_url = "https://data.medicare.gov/resource/rbry-mqwu.json?state=$state";
@@ -36,4 +79,4 @@ function hospital_munger($hospitals, $performances) {
 
   return $performances;
 }
-
+*/
