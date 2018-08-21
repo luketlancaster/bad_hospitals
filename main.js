422dab591f803a11b200c7b9919518be38010bb4
Luke Lancaster <luketlancaster@gmail.com>
2018-08-20 19:10:33 -0500

    Working on getting the JS a little more clean

--- main.js
+++ main.js
@@ -1,50 +1,45 @@
-var states = {"AL":"Alabama","AK":"Alaska","AZ":"Arizona","AR":"Arkansas","CA":"California","CO":"Colorado","CT":"Connecticut","DE":"Delaware","DC":"District Of Columbia","FL":"Florida","GA":"Georgia","HI":"Hawaii","ID":"Idaho","IL":"Illinois","IN":"Indiana","IA":"Iowa","KS":"Kansas","KY":"Kentucky","LA":"Louisiana","ME":"Maine","MD":"Maryland","MA":"Massachusetts","MI":"Michigan","MN":"Minnesota","MS":"Mississippi","MO":"Missouri","MT":"Montana","NE":"Nebraska","NV":"Nevada","NH":"New Hampshire","NJ":"New Jersey","NM":"New Mexico","NY":"New York","NC":"North Carolina","ND":"North Dakota","OH":"Ohio","OK":"Oklahoma","OR":"Oregon","PA":"Pennsylvania","PR":"Puerto Rico","RI":"Rhode Island","SC":"South Carolina","SD":"South Dakota","TN":"Tennessee","TX":"Texas","UT":"Utah","VT":"Vermont","VA":"Virginia","WA":"Washington","WV":"West Virginia","WI":"Wisconsin","WY":"Wyoming"}
-
-
-
 $(document).ready(function() {
-  var stateSelect = $('#stateSelect');
-  var button = $('#getData');
+    var states = {"AL":"Alabama","AK":"Alaska","AZ":"Arizona","AR":"Arkansas","CA":"California","CO":"Colorado","CT":"Connecticut","DE":"Delaware","DC":"District Of Columbia","FL":"Florida","GA":"Georgia","HI":"Hawaii","ID":"Idaho","IL":"Illinois","IN":"Indiana","IA":"Iowa","KS":"Kansas","KY":"Kentucky","LA":"Louisiana","ME":"Maine","MD":"Maryland","MA":"Massachusetts","MI":"Michigan","MN":"Minnesota","MS":"Mississippi","MO":"Missouri","MT":"Montana","NE":"Nebraska","NV":"Nevada","NH":"New Hampshire","NJ":"New Jersey","NM":"New Mexico","NY":"New York","NC":"North Carolina","ND":"North Dakota","OH":"Ohio","OK":"Oklahoma","OR":"Oregon","PA":"Pennsylvania","PR":"Puerto Rico","RI":"Rhode Island","SC":"South Carolina","SD":"South Dakota","TN":"Tennessee","TX":"Texas","UT":"Utah","VT":"Vermont","VA":"Virginia","WA":"Washington","WV":"West Virginia","WI":"Wisconsin","WY":"Wyoming"}
+    var stateSelect = $('#stateSelect');
+    var button = $('#getData');
 
-  for (key in states) {
-    var option = '<option value='+key+'>'+states[key]+'</option>';
-    stateSelect.append(option);
-  }
+    for (key in states) {
+        var option = `<option value=${key}>${states[key]}</option>`;
+        stateSelect.append(option);
+    }
 
-  button.click(getHospitalNames);
+    button.click(getHospitalNames);
+});
 
-  function getHospitalNames(event) {
-    event.preventDefault();
+function getHospitalNames(e) {
+    e.preventDefault();
 
-    var state = stateSelect.val();
+    var state = $('#stateSelect').val();
 
     $.ajax({
-      type: "POST",
-      url: "index.php",
-      data: {state: state},
-      success: attachHospitals
+        type: "POST",
+        url: "index.php",
+        data: { state: state },
+        success: attachHospitals
     });
-  }
+}
 
-  function attachHospitals(hospitals) {
+function attachHospitals(hospitals) {
     var hospitals = $.parseJSON(hospitals);
     var hospitalDiv = $("#hospitals");
     var hospitalCountDiv = $("#hospitalCount");
 
     hospitalDiv.html('');
-    hospitalCountDiv.html('<h2>There are <b>' + hospitals.length + '</b> Scary Hospitals in ' + stateSelect.val() +'<h2>');
+    hospitalCountDiv.html(`<h2>There are <b>${hospitals.length}</b> Scary Hospitals in ${stateSelect.val()}<h2>`);
     hospitals.forEach(function(hospital) {
-      hospitalDiv.append(createTemplate(hospital));
+        hospitalDiv.append(createTemplate(hospital));
     });
-  }
+}
 
-  function createTemplate(hospital) {
-    var html = "<div class='twelve columns hospital_box'>";
-        html += "<h3>" + hospital.hospital_name + "</h3>";
-        html += "<p>" + hospital.address + ", " + hospital.city + ", ";
-        html += hospital.state + " " + hospital.zip_code + "</p>";
-    
-    return html;
-  }
-
-});
+function createTemplate(hospital) {
+    return
+        `<div class='twelve columns hospital_box'>
+            <h3>${hospital.hospital_name}</h3>
+            <p>${hospital.address}, ${hospital.city}, ${hospital.state} ${hospital.zip_code}</p>
+        </div>`;
+}
